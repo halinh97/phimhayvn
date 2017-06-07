6b29d17076ff475b2cdcdd110889ab132b3799ac
halinh97 <nhatlinhrma@gmail.com>
2017-06-08 05:44:55 +0700

    f

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: Node index.js
--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' :  "postgres://xbmngfbbfqjaku:39fda3c9073f9f06553ca6007e3243674f6a2190a98090611901a12537b429aa@ec2-23-21-220-48.compute-1.amazonaws.com:5432/dd0n5iffp5jof5" 
-
-};
